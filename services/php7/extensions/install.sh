58284ca0aa9655424885585555c0ceedf7bf49b7
miss55 <810204005@qq.com>
2023-01-07 01:23:45 +0800

    Update install.sh
    fixed gd

--- services/php7/extensions/install.sh
+++ services/php7/extensions/install.sh
@@ -158,8 +158,9 @@ fi
 
 if [ -z "${EXTENSIONS##*,gd,*}" ]; then
     echo "---------- Install gd ----------"
-    apk add --no-cache freetype-dev libjpeg-turbo-dev libpng-dev \
-    && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
+    apk add libwebp-dev \
+    && apk add --no-cache freetype-dev libjpeg-turbo-dev libpng-dev \
+    && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/  --enable-gd --with-freetype --with-jpeg --with-webp \
     && docker-php-ext-install ${MC} gd
 fi
 
